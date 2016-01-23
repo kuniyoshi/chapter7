df25b5b8cdfd5867b1265711cf41df71e554b698
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-23 19:26:39 +0900

    Convert indention to spaces

--- c7/src/Rect.h
+++ c7/src/Rect.h
@@ -5,22 +5,22 @@ template< class T >
 class Rect
 {
 private:
-	T left_;
-	T right_;
-	T top_;
-	T bottom_;
+    T left_;
+    T right_;
+    T top_;
+    T bottom_;
 public:
-	Rect();
-	Rect(T left, T right, T top, T bottom);
-	Rect(const Rect< T >& base);
-	T left() const;
-	void left(T new_value);
-	T right() const;
-	void right(T new_value);
-	T top() const;
-	void top(T new_value);
-	T bottom() const;
-	void bottom(T new_value);
+    Rect();
+    Rect(T left, T right, T top, T bottom);
+    Rect(const Rect< T >& base);
+    T left() const;
+    void left(T new_value);
+    T right() const;
+    void right(T new_value);
+    T top() const;
+    void top(T new_value);
+    T bottom() const;
+    void bottom(T new_value);
 };
 
 #endif
