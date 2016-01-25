368f0bbb64f56249a3330cda53209ad1e53d7a8e
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-25 09:04:32 +0900

    Convert indention to spaces

--- c7/src/Point.cpp
+++ c7/src/Point.cpp
@@ -8,45 +8,45 @@ Point::Point(const Point& base, int x, int y)
 
 bool Point::operator==(const Point& other) const
 {
-	return x_ == other.x() && y_ == other.y();
+    return x_ == other.x() && y_ == other.y();
 }
 bool Point::operator!=(const Point& other) const
 {
-	return x_ != other.x() || y_ != other.y();
+    return x_ != other.x() || y_ != other.y();
 }
 Point Point::operator+(const Point& operand) const
 {
-	return Point(x_ + operand.x_, y_ + operand.y_);
+    return Point(x_ + operand.x_, y_ + operand.y_);
 }
 Point Point::operator-(const Point& operand) const
 {
-	return Point(x_ - operand.x_, y_ - operand.y_);
+    return Point(x_ - operand.x_, y_ - operand.y_);
 }
 void Point::operator+=(const Point& operand)
 {
-	x_ += operand.x_;
-	y_ += operand.y_;
+    x_ += operand.x_;
+    y_ += operand.y_;
 }
 
 int Point::x() const { return x_; }
 int Point::y() const { return y_; }
 void Point::set_xy(int x, int y)
 {
-	x_ = x;
-	y_ = y;
+    x_ = x;
+    y_ = y;
 }
 
 bool Point::is_in(const Point& left_top, const Point& right_bottom) const
 {
-	return x_ >= left_top.x()
-		&& x_ <= right_bottom.x()
-		&& y_ >= left_top.y()
-		&& y_ <= right_bottom.y();
+    return x_ >= left_top.x()
+        && x_ <= right_bottom.x()
+        && y_ >= left_top.y()
+        && y_ <= right_bottom.y();
 }
 
 int Point::scalar() const { return abs(x_) + abs(y_); }
 
 int Point::distance(const Point& other) const
 {
-	return std::abs(other.x_ - x_) + std::abs(other.y_ - y_);
+    return std::abs(other.x_ - x_) + std::abs(other.y_ - y_);
 }
--- c7/src/Point.h
+++ c7/src/Point.h
@@ -4,24 +4,24 @@
 class Point
 {
 private:
-	int x_;
-	int y_;
+    int x_;
+    int y_;
 
 public:
-	Point();
-	Point(int x, int y);
-	Point(const Point& base, int x, int y);
-	bool operator==(const Point& other) const;
-	bool operator!=(const Point& other) const;
-	Point operator+(const Point& operand) const;
-	Point operator-(const Point& operand) const;
-	void operator+=(const Point& operand);
-	int x() const;
-	int y() const;
-	void set_xy(int x, int y);
-	bool is_in(const Point& left_top, const Point& right_bottom) const;
-	int scalar() const;
-	int distance(const Point& other) const;
+    Point();
+    Point(int x, int y);
+    Point(const Point& base, int x, int y);
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
+    Point operator+(const Point& operand) const;
+    Point operator-(const Point& operand) const;
+    void operator+=(const Point& operand);
+    int x() const;
+    int y() const;
+    void set_xy(int x, int y);
+    bool is_in(const Point& left_top, const Point& right_bottom) const;
+    int scalar() const;
+    int distance(const Point& other) const;
 };
 
 #endif
