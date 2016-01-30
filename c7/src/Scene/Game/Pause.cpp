5fdc814ec78af12c4be2258873ba96f2b7f2bc43
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-31 08:11:01 +0900

    Convert from tab to spaces

--- c7/src/Scene/Game/Pause.cpp
+++ c7/src/Scene/Game/Pause.cpp
@@ -27,37 +27,37 @@ Controller::Game::SceneName Pause::name() const { return Name_; }
 
 void Pause::update(State* state, Controller::Game::SceneName* next_scene_name)
 {
-	Controller::Game::SceneName next_candidates[]
-	= {Controller::Game::SceneNoStateTitle, Controller::Game::ScenePlay};
+    Controller::Game::SceneName next_candidates[]
+    = {Controller::Game::SceneNoStateTitle, Controller::Game::ScenePlay};
 
-	GameLib::Framework f = GameLib::Framework::instance();
-	f.drawDebugString(0, 0, "Pausing...", 0xffffffff);
+    GameLib::Framework f = GameLib::Framework::instance();
+    f.drawDebugString(0, 0, "Pausing...", 0xffffffff);
 
-	if (f.isKeyTriggered('w'))
-	{
-		--looped_index_;
-	}
-	else if (f.isKeyTriggered('z'))
-	{
-		++looped_index_;
-	}
+    if (f.isKeyTriggered('w'))
+    {
+        --looped_index_;
+    }
+    else if (f.isKeyTriggered('z'))
+    {
+        ++looped_index_;
+    }
 
-	f.drawDebugString(0, 4, "Select Next Scene", 0xff000000);
-	f.drawDebugString(1, 5, "Go to Title", 0xff000000);
-	f.drawDebugString(1, 6, "Go back to Play", 0xff000000);
-	f.drawDebugString(0, looped_index_ + 5, ">", 0xff000000);
+    f.drawDebugString(0, 4, "Select Next Scene", 0xff000000);
+    f.drawDebugString(1, 5, "Go to Title", 0xff000000);
+    f.drawDebugString(1, 6, "Go back to Play", 0xff000000);
+    f.drawDebugString(0, looped_index_ + 5, ">", 0xff000000);
 
-	if (f.isKeyTriggered(' '))
-	{
-		*next_scene_name = next_candidates[looped_index_];
+    if (f.isKeyTriggered(' '))
+    {
+        *next_scene_name = next_candidates[looped_index_];
 
-		if (next_candidates[looped_index_] == Controller::Game::ScenePlay)
-		{
-			state->resume();
-		}
-	}
+        if (next_candidates[looped_index_] == Controller::Game::ScenePlay)
+        {
+            state->resume();
+        }
+    }
 
-	state->draw();
+    state->draw();
 }
 
 } // namespace Game
