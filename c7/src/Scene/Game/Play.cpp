07588d14f0c8afaa0e6780af3b736784dc6f0537
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-31 08:12:54 +0900

    Convert from tab to spaces

--- c7/src/Scene/Game/Play.cpp
+++ c7/src/Scene/Game/Play.cpp
@@ -26,41 +26,41 @@ Controller::Game::SceneName Play::name() const { return Name_; }
 
 void Play::update(State* state, Controller::Game::SceneName* next_scene_name)
 {
-	GameLib::Framework f = GameLib::Framework::instance();
-	f.drawDebugString(0, 1, "NOW YOU ARE PLAYING...");
+    GameLib::Framework f = GameLib::Framework::instance();
+    f.drawDebugString(0, 1, "NOW YOU ARE PLAYING...");
 
-	if (f.isKeyTriggered(' '))
-	{
-		*next_scene_name = Controller::Game::ScenePause;
-		state->pause();
-	}
+    if (f.isKeyTriggered(' '))
+    {
+        *next_scene_name = Controller::Game::ScenePause;
+        state->pause();
+    }
 #ifndef NDEBUG
-	else if (f.isKeyTriggered('G'))
-	{
-		*next_scene_name = play_mode_ == Constants::PlayMode1P
-			? Controller::Game::SceneSuccess
-			: Controller::Game::SceneWinLose;
-	}
-	else if (play_mode_ == Constants::PlayMode1P && f.isKeyTriggered('B'))
-	{
-		*next_scene_name = Controller::Game::SceneFailure;
-	}
+    else if (f.isKeyTriggered('G'))
+    {
+        *next_scene_name = play_mode_ == Constants::PlayMode1P
+            ? Controller::Game::SceneSuccess
+            : Controller::Game::SceneWinLose;
+    }
+    else if (play_mode_ == Constants::PlayMode1P && f.isKeyTriggered('B'))
+    {
+        *next_scene_name = Controller::Game::SceneFailure;
+    }
 #endif
 
-	state->update();
+    state->update();
 
-	if (state->does_game_over())
-	{
-		*next_scene_name = play_mode_ == Constants::PlayMode1P
-			? Controller::Game::SceneSuccess
-			: Controller::Game::SceneWinLose;
-	}
-	else if (state->does_game_failure())
-	{
-		*next_scene_name = Controller::Game::SceneFailure;
-	}
+    if (state->does_game_over())
+    {
+        *next_scene_name = play_mode_ == Constants::PlayMode1P
+            ? Controller::Game::SceneSuccess
+            : Controller::Game::SceneWinLose;
+    }
+    else if (state->does_game_failure())
+    {
+        *next_scene_name = Controller::Game::SceneFailure;
+    }
 
-	state->draw();
+    state->draw();
 }
 
 } // namespace Game
