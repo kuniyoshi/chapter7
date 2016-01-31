8ffe155d0870c9cdc08dfa5672cc917233ad20a3
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-31 09:08:04 +0900

    Convert from tab to spaces

--- c7/src/Scene/NoState/Title.cpp
+++ c7/src/Scene/NoState/Title.cpp
@@ -16,44 +16,44 @@ const Controller::NoState::SceneName Title::Name_ = Controller::NoState::SceneTi
 Title::Title()
 : looped_index_(Iterator::Loop(0, 1))
 {
-	new_background_image("data/image/title.dds");
+    new_background_image("data/image/title.dds");
 }
 
 Title::~Title()
 {
-	delete_background_image();
+    delete_background_image();
 }
 
 Controller::NoState::SceneName Title::name() const { return Name_; }
 
-void Title::update(	Controller::NoState::SceneName* next_scene_name,
-					Constants::PlayMode* play_mode)
+void Title::update( Controller::NoState::SceneName* next_scene_name,
+                    Constants::PlayMode* play_mode)
 {
-	const Constants::PlayMode modes[]
-	= {Constants::PlayMode1P, Constants::PlayMode2P};
+    const Constants::PlayMode modes[]
+    = {Constants::PlayMode1P, Constants::PlayMode2P};
 
-	GameLib::Framework f = GameLib::Framework::instance();
+    GameLib::Framework f = GameLib::Framework::instance();
 
-	if (f.isKeyTriggered(' '))
-	{
-		*next_scene_name = Controller::NoState::SceneGame;
-		*play_mode = modes[looped_index_];
-	}
-	else if (f.isKeyTriggered('w'))
-	{
-		++looped_index_;
-	}
-	else if (f.isKeyTriggered('z'))
-	{
-		--looped_index_;
-	}
+    if (f.isKeyTriggered(' '))
+    {
+        *next_scene_name = Controller::NoState::SceneGame;
+        *play_mode = modes[looped_index_];
+    }
+    else if (f.isKeyTriggered('w'))
+    {
+        ++looped_index_;
+    }
+    else if (f.isKeyTriggered('z'))
+    {
+        --looped_index_;
+    }
 
-	draw_background_image();
+    draw_background_image();
 
-	f.drawDebugString(0, 0, "[TITLE]", 0xffffffff);
-	f.drawDebugString(1, 1, "1P Mode", 0xffffffff);
-	f.drawDebugString(1, 2, "2P Mode", 0xffffffff);
-	f.drawDebugString(0, looped_index_ + 1, ">", 0xffffffff);
+    f.drawDebugString(0, 0, "[TITLE]", 0xffffffff);
+    f.drawDebugString(1, 1, "1P Mode", 0xffffffff);
+    f.drawDebugString(1, 2, "2P Mode", 0xffffffff);
+    f.drawDebugString(0, looped_index_ + 1, ">", 0xffffffff);
 }
 
 } // namespace NoState
