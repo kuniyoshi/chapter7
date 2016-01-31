58798d13b2352a8d30209f74461fd0bf31858e41
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-31 09:05:26 +0900

    Convert from tab to spaces

--- c7/src/Scene/NoState/Ending.cpp
+++ c7/src/Scene/NoState/Ending.cpp
@@ -14,29 +14,29 @@ const Controller::NoState::SceneName Ending::Name_ = Controller::NoState::SceneE
 
 Ending::Ending()
 {
-	new_background_image("data/image/ending.dds");
+    new_background_image("data/image/ending.dds");
 }
 
 Ending::~Ending()
 {
-	delete_background_image();
+    delete_background_image();
 }
 
 Controller::NoState::SceneName Ending::name() const { return Name_; }
 
-void Ending::update(	Controller::NoState::SceneName* next_scene_name,
-						Constants::PlayMode*)
+void Ending::update(    Controller::NoState::SceneName* next_scene_name,
+                        Constants::PlayMode*)
 {
-	if (GameLib::Framework::instance().isKeyTriggered(' '))
-	{
-		*next_scene_name = Controller::NoState::SceneTitle;
-	}
+    if (GameLib::Framework::instance().isKeyTriggered(' '))
+    {
+        *next_scene_name = Controller::NoState::SceneTitle;
+    }
 
-	GameLib::Framework::instance().drawDebugString(0, 0, "[ENDING]", 0xffffffff);
-	GameLib::Framework::instance().drawDebugString(0, 1, "... finish the game ..", 0xffffffff);
-	GameLib::Framework::instance().drawDebugString(0, 2, "Space to return to TITLE.", 0xffffffff);
-	
-	draw_background_image();
+    GameLib::Framework::instance().drawDebugString(0, 0, "[ENDING]", 0xffffffff);
+    GameLib::Framework::instance().drawDebugString(0, 1, "... finish the game ..", 0xffffffff);
+    GameLib::Framework::instance().drawDebugString(0, 2, "Space to return to TITLE.", 0xffffffff);
+    
+    draw_background_image();
 }
 
 } // namespace NoState
