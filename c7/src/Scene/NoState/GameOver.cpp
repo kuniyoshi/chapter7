8a8e5eb9c96a92446817e37e654d7548957ac001
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-01-31 09:07:02 +0900

    Convert from tab to spaces

--- c7/src/Scene/NoState/GameOver.cpp
+++ c7/src/Scene/NoState/GameOver.cpp
@@ -15,27 +15,27 @@ const Controller::NoState::SceneName GameOver::Name_
 
 GameOver::GameOver()
 {
-	new_background_image("data/image/game_over.dds");
+    new_background_image("data/image/game_over.dds");
 }
 
 GameOver::~GameOver()
 {
-	delete_background_image();
+    delete_background_image();
 }
 
 Controller::NoState::SceneName GameOver::name() const { return Name_; }
 
-void GameOver::update(	Controller::NoState::SceneName* next_scene_name,
-						Constants::PlayMode*)
+void GameOver::update(  Controller::NoState::SceneName* next_scene_name,
+                        Constants::PlayMode*)
 {
-	if (GameLib::Framework::instance().isKeyTriggered(' '))
-	{
-		*next_scene_name = Controller::NoState::SceneTitle;
-	}
-	
-	GameLib::Framework::instance().drawDebugString(0, 0, "[GAME OVER]", 0xffffffff);
+    if (GameLib::Framework::instance().isKeyTriggered(' '))
+    {
+        *next_scene_name = Controller::NoState::SceneTitle;
+    }
+    
+    GameLib::Framework::instance().drawDebugString(0, 0, "[GAME OVER]", 0xffffffff);
 
-	draw_background_image();
+    draw_background_image();
 }
 
 } // namespace NoState
